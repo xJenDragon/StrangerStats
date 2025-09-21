01db1abffdcec4bab77a0aa6507d15c3915f96b5
Jennifer Mondragon <57730938+xJenDragon@users.noreply.github.com>
2025-09-21 04:07:55 -0500

    Update viz.py

--- helper/viz.py
+++ helper/viz.py
@@ -1,13 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-def plot_hist(df, column="Count", bins=6, title="Histogram"):
-    sns.histplot(df[column], bins=bins)
-    plt.title(title)
-    plt.show()
-
-
 def plot_bar(df, group_col="Area", value_col="Count", agg="mean", title=None):
     grouped = df.groupby(group_col)[value_col].agg(agg).reset_index()
     sns.barplot(data=grouped, x=group_col, y=value_col)
